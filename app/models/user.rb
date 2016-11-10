96d1e1901efd2ec585fb7214013f063ecb40d205
ya1379 <ya1379@gmail.com>
2016-11-10 04:10:31 +0000

    sign up done!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  before_save { self.email = email.downcase }
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :username, presence: true, uniqueness: true, length: {in: 2..20}
+  validates :email, presence: true, uniqueness: true, format: EMAIL_REGEX
+  validates :password, length: { minimum: 6 }
+  #validates_length_of :password, in: 6..20, :on => :create
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161109020935) do
+ActiveRecord::Schema.define(version: 20161109034528) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.string   "encrypted_password"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
